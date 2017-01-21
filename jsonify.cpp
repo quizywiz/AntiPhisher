af3b856943513d3a9bf1f4a6a7f9e15c1e2d747d
Kailash Meiyappan <kailash195@gmail.com>
2017-01-21 15:50:40 -0500

    fixed capital letter bug

--- jsonify.cpp
+++ jsonify.cpp
@@ -5,6 +5,8 @@ int main() {
 	for(int i = 0 ; i < 109; i++) {
 		string a,b;
 		cin >> a >> b;
+		for(char& x : a) if(x>='A' && x<='Z') x = x - 'A' + 'a';
+		for(char& x : b) if(x>='A' && x<='Z') x = x - 'A' + 'a';
 		cout<<"\""<<a<<"\":\""<<b<<"\","<<endl;
 	}
 	cout<<"}";
